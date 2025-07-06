fd386f183c4012d6f52b789d1c9e0611dde408e2
Reynan <reynandan11@gmail.com>
2025-07-06 10:44:52 -0300

    Adição de construtor

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,15 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		Product product = new Product();
-
 		System.out.println("Enter product data:");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+
+		Product product = new Product(name, price, quantity);
 
 		System.out.println("\nProduct data: " + product);
 		
@@ -33,6 +33,7 @@ public class Program {
 		
 		System.out.println("\nUpdate data: " + product);
 
+		sc.close();
 	}
 
 }
